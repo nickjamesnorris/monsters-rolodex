c3dcbe050ab5cd4a6e60caa1db081277c618b9c2
Nick Norris <43795207+100eyez@users.noreply.github.com>
2020-05-17 14:49:42 -0500

    Modified git settings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { CardList } from "./components/card-list/card-list.component";
-import {SearchBox } from './components/search-box/search-box.component'
+import { SearchBox } from "./components/search-box/search-box.component";
 import "./App.css";
 
 class App extends Component {
@@ -12,6 +12,10 @@ class App extends Component {
       searchField: "",
     };
   }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -28,7 +32,7 @@ class App extends Component {
       <div className="App">
         <SearchBox
           placeholder="search monsters"
-          handleChange={(e) => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filtertedMonsters} />
       </div>
