e0b1ee53510f25859f826e4749bdcb25f6d6974d
Nick Norris <43795207+100eyez@users.noreply.github.com>
2020-05-17 21:24:12 -0500

    stylings added

--- src/App.js
+++ src/App.js
@@ -30,12 +30,12 @@ class App extends Component {
     );
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder="search monsters"
           handleChange={this.handleChange}
         />
         <CardList monsters={filtertedMonsters} />
-        <h1>HI!</h1>
       </div>
     );
   }
