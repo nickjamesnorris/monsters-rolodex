bb7599a1a41a1e88aac7b84b91d22a91264e24e1
Nick Norris <43795207+100eyez@users.noreply.github.com>
2020-05-17 21:51:44 -0500

    Updates

---
+++ precache-manifest.248f7945332ad4144f3d07176e5971d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7edb85432c06224c65b7242ba448621",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "330eb7e84dfb23cc237a",
+    "url": "/monsters-rolodex/static/css/main.5042540f.chunk.css"
+  },
+  {
+    "revision": "f83dd82147be52c4d4c9",
+    "url": "/monsters-rolodex/static/js/2.b9294b70.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b9294b70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "330eb7e84dfb23cc237a",
+    "url": "/monsters-rolodex/static/js/main.9052382b.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
